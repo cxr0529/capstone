ba94550b05516bd3e4b6f1e82833534825ac0751
Chuck R <cxr0529@gmail.com>
2024-07-28 16:27:55 -0400

    added new sign up page

--- src/Components/Sign_Up/Sign_Up.js
+++ src/Components/Sign_Up/Sign_Up.js
@@ -1,46 +1,95 @@
-import React from 'react';
-import './Sign_Up.css';
-import { Link } from "react-router-dom";
+import React, { useState } from 'react';
+import './Sign_Up.css'
+import { Link, useNavigate } from 'react-router-dom';
+import { API_URL } from '../../config.js';
 
 const Sign_Up = () => {
+    // State variables using useState hook
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
+    const [password, setPassword] = useState('');
+    const [showerr, setShowerr] = useState(''); // State to show error messages
+    const navigate = useNavigate(); // Navigation hook from react-router
+
+    // Function to handle form submission
+    const register = async (e) => {
+        e.preventDefault(); // Prevent default form submission
+
+        // API Call to register user
+        const response = await fetch(`${API_URL}/api/auth/register`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                name: name,
+                email: email,
+                password: password,
+                phone: phone,
+            }),
+        });
+
+        const json = await response.json(); // Parse the response JSON
+
+        if (json.authtoken) {
+            // Store user data in session storage
+            sessionStorage.setItem("auth-token", json.authtoken);
+            sessionStorage.setItem("name", name);
+            sessionStorage.setItem("phone", phone);
+            sessionStorage.setItem("email", email);
+
+            // Redirect user to home page
+            navigate("/");
+            window.location.reload(); // Refresh the page
+        } else {
+            if (json.errors) {
+                for (const error of json.errors) {
+                    setShowerr(error.msg); // Show error messages
+                }
+            } else {
+                setShowerr(json.error);
+            }
+        }
+    };
+
     return (
-        <div>
-            <div className="container" style={{margintop: '5%'}}>
-                <div className="signup-grid">
-                    <div className="signup-text">
-                        <h1>Sign Up</h1>
-                    </div>
-                    <div className="signup-text1" style={{textalign: "left"}}>
-                        Already a member? <span><Link to="/Login" style={{color: '#2190FF'}}> Login</Link></span>
-                    </div>
-                    <div className="signup-form">
-                        <form>
-                            <div className= "form-group">
-                                <label for="name">Name</label>
-                                <input type="text" name="name" id="name" required className="form-control" placeholder="Enter your name" aria-describedby="helpId" />
-                            </div>
-
-                            <div className="form-group">
-                                <label for="phone">Phone</label>
-                                <input type="tel" name="phone" id="phone" required className="form-control" placeholder="Enter your phone number" aria-describedby="helpId" />
-                            </div>
-
-                            <div className="form-group">
-                                <label for="email">Email</label>
-                                <input type="email" name="email" id="email" required className="form-control" placeholder="Enter your email" aria-describedby="helpId" />
-                            </div>
-
-                            <div className="form-group">
-                                <label for="password">Password</label>
-                                <input name="password" id="password" required className="form-control" placeholder="Enter your password" aria-describedby="helpId" />
-                            </div>
-
-                            <div className="btn-group">
-                                <button type="submit" className="btn btn-primary mb-2 mr-1 waves-effect waves-light">Submit</button>
-                                <button type="reset" className="btn btn-danger mb-2 waves-effect waves-light">Reset</button>
-                            </div>
-                        </form>
-                    </div>
+        <div className="container" style={{marginTop:'10%'}}>
+            <div className="signup-grid">
+                <div className="signup-text">
+                    <h2>Sign Up</h2>
+                </div>
+                <div className="signup-text">
+                    Are you already a member? <span><Link to="/Login" style={{color: "#2190FF"}}>Login Here</Link></span>
+                </div>
+                <br/>
+                <div className="signup-form">
+                    <form method="POST" onSubmit={register}>
+                        <div className="form-group">
+                            <label htmlFor="email">Name</label>
+                            <input value={name} onChange={(e) => setName(e.target.value)} type="name" name="name" id="name" className="form-control" placeholder="Enter your Name" aria-describedby="helpId" />
+                            {showerr && <div className="err" style={{ color: 'red' }}>{showerr}</div>}
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="email">Phone</label>
+                            <input value={phone} onChange={(e) => setPhone(e.target.value)} type="phone" name="phone" id="phone" className="form-control" placeholder="Enter your Phone Number" aria-describedby="helpId" />
+                            {showerr && <div className="err" style={{ color: 'red' }}>{showerr}</div>}
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="email">Email</label>
+                            <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" name="email" id="email" className="form-control" placeholder="Enter your email" aria-describedby="helpId" />
+                            {showerr && <div className="err" style={{ color: 'red' }}>{showerr}</div>}
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="email">Password</label>
+                            <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" name="password" id="password" className="form-control" placeholder="Enter your Password" aria-describedby="helpId" />
+                            {showerr && <div className="err" style={{ color: 'red' }}>{showerr}</div>}
+                        </div>
+                        <div class="btn-group">
+                            <button type="submit" class="btn btn-primary mb-2 mr-1 waves-effect waves-light">Submit</button>
+                            <button type="reset" class="btn btn-danger mb-2 waves-effect waves-light">Reset</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
