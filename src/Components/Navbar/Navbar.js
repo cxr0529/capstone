a9530ae7cbe5913ba35539f1a555d37084a39830
Chuck R <cxr0529@gmail.com>
2024-07-29 13:06:26 -0400

    added instant

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -64,6 +64,9 @@ const Navbar = () => {
           <Link to="/search/doctors">Appointments</Link>
         </li>
         <li className="link">
+          <Link to="/Instant">Instant Consultation</Link>
+        </li>
+        <li className="link">
           <Link to="/healthblog">Health Blog</Link>
         </li>
         <li className="link">
