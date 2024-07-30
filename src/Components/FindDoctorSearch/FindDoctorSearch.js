9f751cd7572c9011c1794ad3189eac7b98a771de
Chuck R <cxr0529@gmail.com>
2024-07-30 14:26:09 -0400

    added search icon

--- src/Components/FindDoctorSearch/FindDoctorSearch.js
+++ src/Components/FindDoctorSearch/FindDoctorSearch.js
@@ -21,7 +21,7 @@ const FindDoctorSearch = () => {
     return (
         <div className='finddoctor'>
             <center>
-                <h1>Find a doctor and Consult instantly</h1>
+                <h1>Find a doctor and Schedule an Appointment</h1>
                 <div>               <i style={{color:'#000000',fontSize:'20rem'}} className="fa fa-user-md"></i>
 </div>                <div className="home-search-container"  style={{display:'flex',justifyContent:'center',alignItems:'center'}}>
                     <div className="doctor-search-box">
@@ -29,11 +29,11 @@ const FindDoctorSearch = () => {
 
                         <input type="text" className="search-doctor-input-box" placeholder="Search doctors, clinics, hospitals, etc." onFocus={() => setDoctorResultHidden(false)} onBlur={() => setDoctorResultHidden(true)} value={searchDoctor} onChange={(e) => setSearchDoctor(e.target.value)} />
                         
-                        <div className="findiconimg"><img className='findIcon' src={process.env.PUBLIC_URL + '/images/search.svg'} alt=""/></div>
+                        <div className="findiconimg"><img className='findIcon' src={process.env.PUBLIC_URL + '/images/search.jpg'} alt=""/></div>
                         <div className="search-doctor-input-results" hidden={doctorResultHidden}>
                             {
                                 specialities.map(speciality => <div className="search-doctor-result-item" key={speciality} onMouseDown={() => handleDoctorSelect(speciality)}>
-                                    <span><img src={process.env.PUBLIC_URL + '/images/search.svg'} alt="" style={{height:"10px", width:"10px"}} width="12" /></span>
+                                    <span><img src={process.env.PUBLIC_URL + '/images/search.jpg'} alt="" style={{height:"10px", width:"10px"}} width="12" /></span>
                                     <span>{speciality}</span>
                                     <span>SPECIALITY</span>
                                 </div>)
