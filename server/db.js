1f7fdf54784d3287ac5defea0d4e8d79e3043fd8
Chuck R <cxr0529@gmail.com>
2024-08-02 10:33:32 -0400

    fixed doc card

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:MTI4MS1jeHIwNTI5@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:OTE3Ni1jeHIwNTI5@127.0.0.1:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
